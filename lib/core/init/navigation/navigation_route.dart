5509dcf4679e03db5f4f95ceb1a505f996b9452e
ramazan <ramazan.aksoy1907@hotmail.com>
2023-02-16 10:49:47 +0300

    architecture template created

---
+++ lib/core/init/navigation/navigation_route.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../view/widget/bottom_navigation_bar.dart';
+import '../../components/not_found_navigation_widget.dart';
+import '../../constants/navigation/navigation_constants.dart';
+
+class NavigationRoute {
+  NavigationRoute._init();
+  static final NavigationRoute _instance = NavigationRoute._init();
+  static NavigationRoute get instance => _instance;
+
+  Route<dynamic> generateRoute(RouteSettings args) {
+    switch (args.name) {
+      case NavigationConstants.DEFAULT:
+        return normalNavigate(const Tabbar(), NavigationConstants.DEFAULT);
+      /*
+      case NavigationConstants.TEST_VIEW:
+        return normalNavigate(TestsView(), NavigationConstants.TEST_VIEW);
+
+      case NavigationConstants.BUY_VIEW:
+        return normalNavigate(const BuyView(), NavigationConstants.BUY_VIEW);
+
+      case NavigationConstants.ON_BOARD:
+        return normalNavigate(const OnBoardView(), NavigationConstants.ON_BOARD);
+*/
+      default:
+        return MaterialPageRoute(
+          builder: (context) => const NotFoundNavigationWidget(),
+        );
+    }
+  }
+
+  MaterialPageRoute normalNavigate(Widget widget, String pageName) {
+    return MaterialPageRoute(
+      builder: (context) => widget,
+      //analytciste görülecek olan sayfa ismi için pageName veriyoruz
+      settings: RouteSettings(name: pageName),
+    );
+  }
+}
---
+++ lib/view/widget/bottom_navigation_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants/themes/colors.dart';
+import '../../utils/text_styles.dart';
+
+class Tabbar extends StatefulWidget {
+  const Tabbar({super.key});
+
+  @override
+  State<Tabbar> createState() => _TabbarState();
+}
+
+class _TabbarState extends State<Tabbar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Favori',
+      style: optionStyle,
+    ),
+    Text(
+      'Category',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedLabelStyle: Styles.normalBoldFontStyle(),
+        unselectedLabelStyle: Styles.normalFontStyle(),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favori',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Category',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.themeColor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
